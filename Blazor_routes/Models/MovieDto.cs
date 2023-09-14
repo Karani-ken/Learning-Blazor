89288713a2d1a74f9575e4fb6bc3d77b0cc9ac2e
karani-ken <academice79@gmail.com>
2023-09-14 15:09:49 +0300

    Add project files.

---
+++ Blazor_routes/Models/MovieDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor_routes.Models
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+        public int Rating { get; set; }
+        public string Poster { get; set; } = string.Empty;
+    }
+}
